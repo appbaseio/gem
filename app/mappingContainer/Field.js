f4c9dec64e93f6484494584217613925f540990f
farhan chauhan <farhan687@gmail.com>
2016-10-11 22:22:40 +0530

    Nested properties implemented

--- app/mappingContainer/Field.js
+++ app/mappingContainer/Field.js
@@ -9,7 +9,7 @@ export class Field extends Component {
     this.state = {
       rows: [],
       modifiedField: [],
-      properties: {}
+      fieldRecord: {}
     };
     this.defaultRow = {
       name: 'sample',
@@ -19,16 +19,16 @@ export class Field extends Component {
     this.setModifiedField = this.setModifiedField.bind(this);
   }
   componentWillMount() {
-    if(this.props.mappings[this.props.singleType].properties) {
+    if(this.props.fieldRecord) {
       this.setState({
-        properties: this.props.mappings[this.props.singleType].properties
+        fieldRecord: this.props.fieldRecord
       });
     }
   }
   setModifiedField(oldfieldName, newfield) {
     let modifiedField = this.state.modifiedField;
-    let properties = this.state.properties;
-    let fields = properties[this.props.field].fields;
+    let fieldRecord = this.state.fieldRecord;
+    let fields = fieldRecord.fields;
     let rows = this.state.rows;
     rows = rows.filter((row) => {
       row.fieldName !== oldfieldName
@@ -42,11 +42,11 @@ export class Field extends Component {
       type: newfield.type,
       index: newfield.index,
     };
-    properties[this.props.field].fields = fields;
+    fieldRecord.fields = fields;
     modifiedField.push(newfield);
     this.setState({
       modifiedField: modifiedField,
-      properties: properties,
+      fieldRecord: fieldRecord,
       rows: rows
     });
   }
@@ -95,33 +95,57 @@ export class Field extends Component {
   }
   submit() {
     let request = {
-      properties: this.state.properties
+      properties: {
+        [this.props.field]: this.state.fieldRecord
+      }
     };
     dataOperation.updateMapping(request, this.props.singleType).then((res) => {
       alert(JSON.stringify(res));
     });
   }
+  multipleField() {
+    let fieldList = this.props.fieldList.filter((item, index) => this.props.id === item.parent);
+    return fieldList.map((item, index) => {
+      return (<Field 
+        mappings = {this.props.mappings}
+        fieldRecord = {item.mappingObj}
+        field = {item.field}
+        singleType = {item.singleType}
+        className="singleProperty col-xs-12" 
+        fieldList = {this.props.fieldList}
+        id = {item.id}
+        parent = {item.parent}
+        key = {index} >
+      </Field>);
+    })
+  }
   render() {
-    let properties = this.state.properties;
-    let fields = properties[this.props.field].fields;
-    let submit;
+    let fieldRecord = this.state.fieldRecord;
+    let fields = fieldRecord.fields;
+    let submit, addRow;
     if(this.state.modifiedField.length) {
       submit = (<a className="btn btn-success pull-right" onClick={() => this.submit() }>Submit</a>);
+    }
+    if(fieldRecord.type) {
+      addRow = (<a className="btn btn-primary pull-right" onClick={() => this.addField() }>Add Field</a>);
     }
     return (<div className="singleProperty col-xs-12">
       <h3 className='title row'>
         <span>
           {this.props.field} ({this.props.singleType})
         </span>
-        <span className={'datatype '+ (!properties[this.props.field].type ? ' hide ' : '')}>
-          {properties[this.props.field].type}
+        <span className={'datatype '+ (!fieldRecord.type ? ' hide ' : '')}>
+          {fieldRecord.type}
         </span>
-        <a className="btn btn-primary pull-right" onClick={() => this.addField() }>Add Field</a>
+        {addRow}
         {submit}
       </h3>
       <div className="fieldContent row">
         {this.addRows()}
         {this.fieldContent(fields)}
+        <div className="col-xs-12 nestedField">
+          {this.multipleField()}
+        </div>
       </div>
     </div>);
   }
--- app/mappingContainer/Fields.js
+++ app/mappingContainer/Fields.js
@@ -10,29 +10,74 @@ export class Fields extends Component {
   componentWillMount() {
     
   }
-  generateFieldList() {
-    let fieldList = [];
-    if(this.props.selectedType && this.props.mappings) {
+  arrangeFields() {
+    this.fieldList = [];
+    if(this.props.selectedType && this.props.selectedType.length && this.props.mappings) {
       let index = 0;
       for(let singleType in this.props.mappings) {
         if(this.props.selectedType.indexOf(singleType) > -1) {
           let fields = this.props.mappings[singleType].properties;
           for(let field in fields) {
             index++;
-            let markup = (<Field 
-              mappings = {this.props.mappings}
-              field = {field}
-              singleType = {singleType}
-              className="singleProperty col-xs-12" key = {index}></Field>);
-            fieldList.push(markup);
+            let mappingObj = fields[field];
+            let obj = this.isNested(index, 0, mappingObj, field, singleType);
+            this.fieldList.push(obj); 
           }
         }
-      }  
+      }
+      return this.resolveList();
     }
-    return fieldList;
+  }
+  isNested(index, parent, mappingObj, field, singleType) {
+    let obj = {
+      parent: parent,
+      id:  index,
+      field: field,
+      singleType: singleType,
+      mappingObj: mappingObj
+    };    
+    if(mappingObj.hasOwnProperty('properties')) {
+      obj.resolved = false;
+    } else {
+      obj.resolved = true;
+    }
+    return obj;
+  }
+  resolveList() {
+    let list = this.fieldList.filter((item) => !item.resolved);
+    if(list.length) {
+      let index = this.fieldList.length;
+      list.forEach((item) => {
+        this.fieldList[item.id-1].resolved = true;
+        for(let field in item.mappingObj.properties) {
+          index++;
+          let mappingObj = item.mappingObj.properties[field];
+          let obj = this.isNested(index, item.id, mappingObj, field, item.singleType);
+          this.fieldList.push(obj); 
+        }
+      });
+      return this.resolveList();
+    } else {
+      let fieldList = this.fieldList.filter((item, index) => item.parent === 0);
+      return fieldList.map((item, index) => {
+        return (<Field 
+          mappings = {this.props.mappings}
+          fieldRecord = {item.mappingObj}
+          field = {item.field}
+          singleType = {item.singleType}
+          className="singleProperty col-xs-12" 
+          fieldList = {this.fieldList}
+          id = {item.id}
+          parent = {item.parent}
+          key = {index}>
+        </Field>);
+      });
+
+    }
   }
   render() {
-    let fieldList = this.generateFieldList();
+    let fieldList = this.arrangeFields();
+    console.log(fieldList);
     return (
       <div className="FieldContainer">
         {fieldList}

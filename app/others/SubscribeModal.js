a8c45e5038549d101f6bf6d9e44fa4429be1fd10
farhan687 <farhan687@gmail.com>
2016-11-28 18:24:16 +0530

    Subscribe modal auto popup changes
    Watch focus and blur event of windows and hold the modal if tab is inactive.

--- app/others/SubscribeModal.js
+++ app/others/SubscribeModal.js
@@ -5,108 +5,136 @@ import { Modal } from 'react-bootstrap';
 import { authOperation, authEmitter } from '../service/authOperation';
 
 export class SubscribeModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showModal: false,
-      profile: false,
-      subscribeOption: 'major'
-    };
-    this.options = {
-      option1: {
-        value: 'major',
-        text: 'New GEM releases'
-      },
-      option2: {
-        value: 'all',
-        text: 'Limited major updates'
-      }
-    }
-    this.timer = 1;
-    this.init();
-  }
-  init() {
-    authEmitter.addListener('profile', (data) => {
-      this.setState({
-        profile: data
-      });
-    });
-    if(storageService.get('popuptimer')) {
-      this.timer = parseInt(storageService.get('popuptimer'));
-    }
-    setTimeout(() => {
-      if(!this.state.profile) {
-        this.open();
-      }
-    }, 1000*60*this.timer);
-  }
-  close() {
-    this.internalClose = true;
-    storageService.set('popuptimer', this.timer+5);
-    this.setState({ showModal: false });
-  }
-  open() {
-    if(!this.state.profile) {
-      if(!$('.fade.in.modal').length) {
-        this.setState({ showModal: true });
-      } else {
-        setTimeout(() => {
-          this.open();
-        }, 1000*10);
-      }
-    }
-  }
-  subscribe() {
-    authOperation.login(this.state.subscribeOption);
-  }
-  subscribeOptionChange(value) {
-    this.setState({
-      subscribeOption: value
-    });
-  }
-  showIcon() {
-    let icon = (<i className="fa fa-envelope-o"></i>);
-    if(this.state.profile) {
-      icon = (<i className="fa fa-check"></i>);
-    }
-    return icon;
-  }
-  render() {
-    return (
-      <div>
-        <a title="Subscribe to updates" className="subscribe" href="javascript:void;" onClick={() => this.open()}>
-          {this.showIcon()}
-        </a>
-        <Modal className="modal-info" show={this.state.showModal} onHide={() => this.close()}>
-          <Modal.Header closeButton>
-            <Modal.Title>Be in the know about major updates!</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <div className="row">
-              <div className="col-xs-12">
-                <div className="row">
-                  <div className="col-xs-12 single-option">
-                    <label className="radio-inline">
-                      <input type="radio"
-                        checked={this.state.subscribeOption === this.options.option1.value}
-                        onChange={() => this.subscribeOptionChange(this.options.option1.value)}
-                        name="subscribeOption" id="subscribeOption" value={this.options.option1.value} /> {this.options.option1.text}
-                    </label>
-                  </div>
-                  <div className="col-xs-12 single-option">
-                    <label className="radio-inline">
-                      <input type="radio"
-                        checked={this.state.subscribeOption === this.options.option2.value}
-                        onChange={() => this.subscribeOptionChange(this.options.option2.value)}
-                        name="subscribeOption1" id="subscribeOption1" value={this.options.option2.value} /> {this.options.option2.text}
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
-						<div className="col-xs-12 text-center">
-              <button className="btn btn-primary" onClick={() => this.subscribe()}>
-                <i className="fa fa-github"></i> Subscribe with Github
+	constructor(props) {
+		super(props);
+		this.state = {
+			showModal: false,
+			profile: false,
+			subscribeOption: 'major'
+		};
+		this.options = {
+			option1: {
+				value: 'major',
+				text: 'New GEM releases'
+			},
+			option2: {
+				value: 'all',
+				text: 'Limited major updates'
+			}
+		}
+		this.timer = 1;
+		this.activetab = true;
+		this.holdSubscribe = false;
+		this.init();
+	}
+	init() {
+		authEmitter.addListener('profile', (data) => {
+			this.setState({
+				profile: data
+			});
+		});
+		if (storageService.get('popuptimer')) {
+			this.timer = parseInt(storageService.get('popuptimer'));
+		}
+		setTimeout(() => {
+			if (!this.state.profile) {
+				this.open();
+			}
+		}, 1000 * 60 * this.timer);
+
+		$(window).focus(function() {
+			this.activetab = true;
+			setTimeout(() => {
+				if (!this.state.profile && this.holdSubscribe) {
+					this.open();
+				}
+			}, 1000 * 10 * this.timer);
+		}.bind(this));
+
+		$(window).blur(function() {
+			this.activetab = false;
+		}.bind(this));
+	}
+	close() {
+		this.internalClose = true;
+		storageService.set('popuptimer', this.timer + 5);
+		this.setState({ showModal: false });
+	}
+	open() {
+		if (!this.state.profile) {
+			if (!$('.fade.in.modal').length) {
+				if(this.activetab) {
+					this.setState({ showModal: true });
+				} else {
+					this.holdSubscribe = true;
+				}
+			} else {
+				setTimeout(() => {
+					this.open();
+				}, 1000 * 10);
+			}
+		}
+	}
+	subscribe() {
+		authOperation.login(this.state.subscribeOption);
+	}
+	subscribeOptionChange(value) {
+		this.setState({
+			subscribeOption: value
+		});
+	}
+	showIcon() {
+		let icon = (<i className="fa fa-envelope-o"></i>);
+		if (this.state.profile) {
+			icon = (<i className="fa fa-check"></i>);
+		}
+		return icon;
+	}
+	render() {
+		return (
+		<div>
+			<a title="Subscribe to updates" className="subscribe" href="javascript:void;" onClick={() => this.open()}>
+				{this.showIcon()}
+			</a>
+			<Modal className="modal-info" show={this.state.showModal} onHide={() => this.close()}>
+			<Modal.Header closeButton>
+				<Modal.Title>Be in the know about major updates!</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<div className="row">
+					<div className="col-xs-12">
+						<div className="row">
+							<div className="col-xs-12 single-option">
+								<label className="radio-inline">
+								<input type="radio"
+									checked={this.state.subscribeOption === this.options.option1.value}
+									onChange={() => this.subscribeOptionChange(this.options.option1.value)}
+									name="subscribeOption" id="subscribeOption" value={this.options.option1.value} /> {this.options.option1.text}
+								</label>
+							</div>
+							<div className="col-xs-12 single-option">
+								<label className="radio-inline">
+								<input type="radio"
+									checked={this.state.subscribeOption === this.options.option2.value}
+									onChange={() => this.subscribeOptionChange(this.options.option2.value)}
+									name="subscribeOption1" id="subscribeOption1" value={this.options.option2.value} /> {this.options.option2.text}
+								</label>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="col-xs-12 text-center">
+					<button className="btn btn-primary" onClick={() => this.subscribe()}>
+						<i className="fa fa-github"></i> Subscribe with Github
+					</button>
+				</div>
+				</Modal.Body>
+			</Modal>
+		</div>
+		)
+	}
+}
+me="fa fa-github"></i> Subscribe with Github
               </button>
             </div>
           </Modal.Body>

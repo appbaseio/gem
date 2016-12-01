9dcde9cb746994ee829388b67da808aa89359e63
farhan687 <farhan687@gmail.com>
2016-12-01 21:48:34 +0530

    Build

--- _site/app/mappingContainer/SingleField.js
+++ _site/app/mappingContainer/SingleField.js
@@ -1,127 +1,125 @@
-import { default as React, Component } from 'react';
+import {default as React,Component} from 'react';
 import { render } from 'react-dom';
 import { dataOperation } from '../service/DataOperation';
 import { Editable } from './Editable';
+import {findRegisteredAnalyzer} from '../others/helper';
 
 export class SingleField extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      defaultEdit: false
-    };
-    this.modified = {};
-    this.excludeProp = ['index', 'analyzer'];
-    this.editCb = this.editCb.bind(this);
-  }
-  componentWillMount() {
-    if(this.props.defaultEdit) {
-    }
-  }
-  allowEdit() {
-    let editFlag = !this.props.defaultEdit;
-  }
-  deleteField() {
-    var r = confirm('Do you want to delete this field?');
-    if(r) {
-      this.props.deleteField(this.props.field, this.props.singleType);
-    }
-  }
-  editCb(key, value) {
-    this.modified[key] = value;
-    this.setState(this.modified);
-  }
-  validate() {
-    let flag = true;
-    for(let field in this.modified) {
-      if(this.excludeProp.indexOf(field) < 0 && !(this.modified[field] && this.modified[field] !== '')) {
-        flag = false;
-      }
-    }
-    this.excludeProp.forEach((pro) => {
-      if(this.modified[pro] && this.modified[pro] === '') {
-        delete this.modified[pro];
-      }
-    });
-    return flag;
-  }
-  saveEdit() {
-    if(this.validate()) {
-      this.props.setModifiedField(this.props.fieldName, this.modified);
-    }
-  }
-  quitEditable() {
-    this.props.removeField(this.props.fieldName);
-  }
-  operationalBtn() {
-    let operationalBtn;
-    if(this.props.defaultEdit) {
-      operationalBtn = (<span className="operationalBtns">
-        <a onClick={() => this.saveEdit()} className={"btn btn-xs "+(this.props.view === 'mapping' ? 'btn-yellow' : 'btn-primary')}>
-          <i className="fa fa-check"></i>
-        </a>
-        <a onClick={() => this.quitEditable()} className="btn btn-xs btn-grey-bg">
-          <i className="fa fa-times"></i>
-        </a>
-      </span>);
-    }
-    return operationalBtn;
-  }
-  render() {
-    let indexRow, analyzerRow, analyzer, registeredAnalyzers = null;
-    let operationalBtn = this.operationalBtn();
-    if(this.modified.type === 'string' && !(!this.props.defaultEdit && this.props.fieldInfo.index === 'analyzed')) {
-      indexRow = (
-        <span className="fieldIndex col-xs-12 col-sm-4">
-          <Editable 
-            editKey='index'
-            editCb={this.editCb}
-            editValue={this.props.fieldInfo.index} 
-            defaultEdit={this.props.defaultEdit} 
-            placeholder="index type"/>
-        </span>
-      );
-    }
-    if(dataOperation.settings) {
-      try {
-        registeredAnalyzers = dataOperation.settings[dataOperation.inputState.appname].settings.index.analysis.analyzer;
-      } catch(e) {}
-      registeredAnalyzers = registeredAnalyzers ? Object.keys(registeredAnalyzers) : [];
-      if(registeredAnalyzers.length) {
-        analyzerRow = (
-          <span className="fieldAnalyzer col-xs-12 col-sm-4">
-            <Editable 
-              editKey='analyzer'
-              editCb={this.editCb}
-              editValue={this.props.fieldInfo.analyzer} 
-              defaultEdit={this.props.defaultEdit} 
-              registeredAnalyzers={registeredAnalyzers}
-              placeholder="analyzer"/>
-          </span>
-        );
-      } 
-    }   
-    return (
-      <div className="internalField col-xs-12">
-        <span className="fieldName col-xs-12 col-sm-4">
-          <Editable 
-            editKey='fieldName'
-            editCb={this.editCb}
-            editValue={this.props.fieldName} 
-            defaultEdit={this.props.defaultEdit} 
-            placeholder="Enter fieldname"/>
-        </span>
-        <span className="datatype col-xs-12 col-sm-4">
-          <Editable
-            editKey='type'
-            editCb={this.editCb}
-            editValue={this.props.fieldInfo.type} 
-            defaultEdit={this.props.defaultEdit} 
-            placeholder="datatype"/>
-        </span>
-        {indexRow}
-        {analyzerRow}
-        {operationalBtn}
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			defaultEdit: false
+		};
+		this.modified = {};
+		this.excludeProp = ['index', 'analyzer'];
+		this.editCb = this.editCb.bind(this);
+	}
+	componentWillMount() {
+		if (this.props.defaultEdit) {}
+	}
+	allowEdit() {
+		let editFlag = !this.props.defaultEdit;
+	}
+	deleteField() {
+		var r = confirm('Do you want to delete this field?');
+		if (r) {
+			this.props.deleteField(this.props.field, this.props.singleType);
+		}
+	}
+	editCb(key, value) {
+		this.modified[key] = value;
+		this.setState(this.modified);
+	}
+	validate() {
+		let flag = true;
+		for (let field in this.modified) {
+			if (this.excludeProp.indexOf(field) < 0 && !(this.modified[field] && this.modified[field] !== '')) {
+				flag = false;
+			}
+		}
+		this.excludeProp.forEach((pro) => {
+			if (this.modified[pro] && this.modified[pro] === '') {
+				delete this.modified[pro];
+			}
+		});
+		return flag;
+	}
+	saveEdit() {
+		if (this.validate()) {
+			this.props.setModifiedField(this.props.fieldName, this.modified);
+		}
+	}
+	quitEditable() {
+		this.props.removeField(this.props.fieldName);
+	}
+	operationalBtn() {
+		let operationalBtn;
+		if (this.props.defaultEdit) {
+			operationalBtn = (<span className="operationalBtns">
+		<a onClick={() => this.saveEdit()} className={"btn btn-xs "+(this.props.view === 'mapping' ? 'btn-yellow' : 'btn-primary')}>
+		  <i className="fa fa-check"></i>
+		</a>
+		<a onClick={() => this.quitEditable()} className="btn btn-xs btn-grey-bg">
+		  <i className="fa fa-times"></i>
+		</a>
+	  </span>);
+		}
+		return operationalBtn;
+	}
+	render() {
+		let indexRow, analyzerRow, analyzer, registeredAnalyzers = null;
+		let operationalBtn = this.operationalBtn();
+		if (this.modified.type === 'string' && !(!this.props.defaultEdit && this.props.fieldInfo.index === 'analyzed')) {
+			indexRow = (
+				<span className="fieldIndex col-xs-12 col-sm-4">
+		  <Editable 
+			editKey='index'
+			editCb={this.editCb}
+			editValue={this.props.fieldInfo.index} 
+			defaultEdit={this.props.defaultEdit} 
+			placeholder="index type"/>
+		</span>
+			);
+		}
+		if (dataOperation.settings) {
+			registeredAnalyzers = findRegisteredAnalyzer(dataOperation);
+			registeredAnalyzers = registeredAnalyzers ? Object.keys(registeredAnalyzers) : [];
+			if (registeredAnalyzers.length) {
+				analyzerRow = (
+					<span className="fieldAnalyzer col-xs-12 col-sm-4">
+			<Editable 
+			  editKey='analyzer'
+			  editCb={this.editCb}
+			  editValue={this.props.fieldInfo.analyzer} 
+			  defaultEdit={this.props.defaultEdit} 
+			  registeredAnalyzers={registeredAnalyzers}
+			  placeholder="analyzer"/>
+		  </span>
+				);
+			}
+		}
+		return (
+			<div className="internalField col-xs-12">
+		<span className="fieldName col-xs-12 col-sm-4">
+		  <Editable 
+			editKey='fieldName'
+			editCb={this.editCb}
+			editValue={this.props.fieldName} 
+			defaultEdit={this.props.defaultEdit} 
+			placeholder="Enter fieldname"/>
+		</span>
+		<span className="datatype col-xs-12 col-sm-4">
+		  <Editable
+			editKey='type'
+			editCb={this.editCb}
+			editValue={this.props.fieldInfo.type} 
+			defaultEdit={this.props.defaultEdit} 
+			placeholder="datatype"/>
+		</span>
+		{indexRow}
+		{analyzerRow}
+		{operationalBtn}
+	  </div>
+		);
+	}
 }
--- _site/app/mappingContainer/importSettings/ImportContainer.js
+++ _site/app/mappingContainer/importSettings/ImportContainer.js
@@ -1,83 +1,76 @@
-import { default as React, Component } from 'react';
+import {default as React, Component } from 'react';
 import { render } from 'react-dom';
 import { dataOperation } from '../../service/DataOperation';
 import { ErrorModal } from '../../others/ErrorModal';
 import { JsonImport } from './JsonImport';
+import { closeError } from '../../others/helper';
 
 export class ImportContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: {
-        title: null,
-        message: null
-      },
-      successMessage: null
-    };
-    this.settingsObj = null;
-    this.handleUpdate = this.handleUpdate.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.updateSuccess = this.updateSuccess.bind(this);
-  }
-  updateSuccess() {
-    this.setState({
-      successMessage: 'Setting is updated successfully!'
-    }, function() {
-      setTimeout(() => {
-        this.props.getMapping();
-        this.props.close();
-      }, 1000*3);
-    }.bind(this));
-  }
-  handleUpdate(jsonInput, selectedType, importType='data') {
-    this.settingsObj = {
-      analysis: jsonInput
-    };
-    this.closeError = this.closeError.bind(this);
-  }
-  handleSubmit() {
-    console.log(this.settingsObj);
-    dataOperation.ocIndex('_close').done((res) => {
-      dataOperation.updateSettings(this.settingsObj).done((res) => {
-        dataOperation.ocIndex('_open').done((res) => {
-          this.updateSuccess();
-        }).fail((res) => {
-          this.errorShow();
-        });    
-      }).fail((res) => {
-        this.errorShow();
-      }); 
-    }).fail((res) => {
-      this.errorShow();
-    });
-  }
-  errorShow() {
-    let error = this.state.error;
-    error.title = 'Error';
-    error.message = res.responseText;
-    this.setState({
-      error: error
-    });
-  }
-  closeError() {
-    let error = this.state.error;
-    error.title = null;
-    error.message = null;
-    this.setState({
-      error: error
-    });
-  }
-  render() {
-    return (<div className="row" id="ImportContainer">
-      <JsonImport 
-        mappings={this.props.mappings} 
-        handleUpdate={this.handleUpdate} 
-        handleSubmit={this.handleSubmit} 
-        updateSuccess={this.updateSuccess}
-        successMessage={this.state.successMessage}
-        getMapping={this.props.getMapping}
-      />
-      <ErrorModal {...this.state.error} closeError={this.closeError} />
-    </div>);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: {
+				title: null,
+				message: null
+			},
+			successMessage: null
+		};
+		this.settingsObj = null;
+		this.handleUpdate = this.handleUpdate.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.updateSuccess = this.updateSuccess.bind(this);
+		this.closeError = closeError.bind(this);
+	}
+	updateSuccess() {
+		this.setState({
+			successMessage: 'Setting is updated successfully!'
+		}, function() {
+			setTimeout(() => {
+				this.props.getMapping();
+				this.props.close();
+			}, 1000 * 3);
+		}.bind(this));
+	}
+	handleUpdate(jsonInput, selectedType, importType = 'data') {
+		this.settingsObj = {
+			analysis: jsonInput
+		};
+	}
+	handleSubmit() {
+		console.log(this.settingsObj);
+		dataOperation.ocIndex('_close').done((res) => {
+			dataOperation.updateSettings(this.settingsObj).done((res) => {
+				dataOperation.ocIndex('_open').done((res) => {
+					this.updateSuccess();
+				}).fail((res) => {
+					this.errorShow();
+				});
+			}).fail((res) => {
+				this.errorShow();
+			});
+		}).fail((res) => {
+			this.errorShow();
+		});
+	}
+	errorShow() {
+		let error = this.state.error;
+		error.title = 'Error';
+		error.message = res.responseText;
+		this.setState({
+			error: error
+		});
+	}
+	render() {
+		return (<div className="row" id="ImportContainer">
+			<JsonImport 
+				mappings={this.props.mappings} 
+				handleUpdate={this.handleUpdate} 
+				handleSubmit={this.handleSubmit} 
+				updateSuccess={this.updateSuccess}
+				successMessage={this.state.successMessage}
+				getMapping={this.props.getMapping}
+			/>
+			<ErrorModal {...this.state.error} closeError={this.closeError} />
+		</div>);
+	}
 }
